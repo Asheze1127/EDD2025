db10d79b89aadbb63fe5e8cef25475be866fa3ec
Asheze1127 <m.k.h11272005@gmail.com>
2025-08-13 00:27:42 +0900

    0813:コンポーネントの整備を少し進めました。

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+function LoginPage() {
+    return(
+        <div>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
